abae3c81b88ca22fa53a924bbf2ae4a9f349e9fb
KhaledKammoun <khaledsfax03@gmail.com>
2023-11-08 23:58:39 +0100

    C++

--- Codeforces_Round_147_Div.2/B-Sort-the-Subarray.cpp
+++ Codeforces_Round_147_Div.2/B-Sort-the-Subarray.cpp
@@ -6,45 +6,28 @@ int main(){
     for (int i = 0 ; i<t ; i++){
         int n ;
         cin>>n ;
+        int tab[200001] = {0} ;
         for (int j = 0 ; j<n ;j++){
-            int x ;
-            cin>>x;
+            
+            cin>>tab[j] ;
         }
-        vector<pair<int,int>> v ;
-        int max_size_1 = -1, max_size_2 = -1; 
-        int x_1 = 1, y_1 = 1,x_2 = 1, y_2 = 1 ;
-        int x_var = -1 ;
-        int x = 1, y = 1, prev_var = 0 ;
+        int x = 1, y = 1 ;
+        int var = -1,prev_var = 200001 ;
+        int tab1[200001] = {0} ;
         for (int j = 0 ; j<n ;j++){
-            int var ;
             cin>>var ;
-            if (prev_var != 0){
-                if (var < prev_var){
-                    x = y ;
-                    x_var = var ;
-                }
-                if (((y_1 - x_1) >= max_size_1) && (x_var != var)){
-                    x_1 = x ;
-                    y_1 = y ;
-                    max_size_1 = y_1 - x_1 ;
-                }
-                if (((y_2 - x_2) >= max_size_2) && (x_var == var)){
-                    x_2 = x ;
-                    y_2 = y ;
-                    max_size_2 = y_2 - x_2 ;
-                }
-                
+            if (prev_var <= var){
+                if (x == 1)
+                    x = j ;
+                else 
+                    y = j + 1;
             }
-            else 
-                x_var = var ;
+            
             prev_var = var ;
-            y++ ;
         }
-        if (y_1 - x_1 >= y_2 - x_2)
-            cout<<x_1<<" "<<y_1<<endl ;
-        else
-            cout<<x_2<<" "<<y_2<<endl ;
-        
+        if (prev_var <= var)
+            y = n ;
+        cout<<x<<" "<<y<<endl ;
             
     }
 
