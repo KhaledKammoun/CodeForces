f6f49c5aa43b2aa99dd45c32153a615f5703501a
KhaledKammoun <khaledsfax03@gmail.com>
2023-11-10 23:45:05 +0100

    C++

--- Codeforces_Round_147_Div.2/B-Sort-the-Subarray.cpp
+++ Codeforces_Round_147_Div.2/B-Sort-the-Subarray.cpp
@@ -12,22 +12,25 @@ int main(){
             cin>>tab[j] ;
         }
         int x = 1, y = 1 ;
-        int var = -1,prev_var = 200001 ;
+        bool y_move = false ;
         int tab1[200001] = {0} ;
         for (int j = 0 ; j<n ;j++){
-            cin>>var ;
-            if (prev_var <= var){
-                if (x == 1)
-                    x = j ;
-                else 
-                    y = j + 1;
+            cin>>tab1[j] ;
+            if (tab1[j] != tab[j]){
+                x = j ;
+                y_move = true ;
             }
-            
-            prev_var = var ;
+            else 
+                y_move = false ;
+            if (y_move)
+                y = j ;
         }
-        if (prev_var <= var)
-            y = n ;
-        cout<<x<<" "<<y<<endl ;
+        
+        while (x>0 && tab1[x-1] <= tab1[x])
+            x-- ;
+        while (y<n-1 && tab1[y] <= tab1[y+1])
+            y++ ;
+        cout<<x+1<<" "<<y+1<<endl ;
             
     }
 
