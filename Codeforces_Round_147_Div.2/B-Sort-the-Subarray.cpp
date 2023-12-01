c962bfd70d2e12e9f2d98dbfcb5a132bc7a9bbf8
KhaledKammoun <khaledsfax03@gmail.com>
2023-12-02 00:46:12 +0100

    C++

--- Codeforces_Round_147_Div.2/B-Sort-the-Subarray.cpp
+++ Codeforces_Round_147_Div.2/B-Sort-the-Subarray.cpp
@@ -6,31 +6,32 @@ int main(){
     for (int i = 0 ; i<t ; i++){
         int n ;
         cin>>n ;
-        int tab[200001] = {0} ;
-        for (int j = 0 ; j<n ;j++){
-            
-            cin>>tab[j] ;
-        }
-        int x = 1, y = 1 ;
-        bool y_move = false ;
-        int tab1[200001] = {0} ;
-        for (int j = 0 ; j<n ;j++){
-            cin>>tab1[j] ;
-            if (tab1[j] != tab[j]){
-                x = j ;
-                y_move = true ;
-            }
-            else 
-                y_move = false ;
-            if (y_move)
-                y = j ;
-        }
         
-        while (x>0 && tab1[x-1] <= tab1[x])
-            x-- ;
-        while (y<n-1 && tab1[y] <= tab1[y+1])
-            y++ ;
-        cout<<x+1<<" "<<y+1<<endl ;
+        for (int j = 0 ; j<n ;j++){
+            int var ;
+            cin>>var ;
+        }
+        int var = -1, prev_var = -1 ;
+        int x = 1, y = 1, x_var = 1, y_var = 1 ;
+        
+        for (int j = 0 ; j<n ;j++){
+            cin>>var ;
+            if (prev_var != -1){
+                if (var < prev_var){
+                    if (y - x >= y_var - x_var){
+                        x_var = x ;
+                        y_var = y ;
+                    }
+                    
+                    x = j ;
+                    y = j ;
+                }
+                else 
+                    y = j ;
+            }
+            prev_var = var ;
+        }
+        cout<<x_var+1<<" "<<y_var+1<<endl ;
             
     }
 
