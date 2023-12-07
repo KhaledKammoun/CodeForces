284f2328244d8fa8b001ed62daeb32a6cfcc3c16
KhaledKammoun <khaledsfax03@gmail.com>
2023-12-08 00:42:54 +0100

    C++

--- ISIMS-CPC/#5-too_lazy_to_search/C.Yarik_and_Array.cpp
+++ ISIMS-CPC/#5-too_lazy_to_search/C.Yarik_and_Array.cpp
@@ -6,31 +6,45 @@ int main(){
     cin>>t ;
     while (t--){
         cin>>n ;
+        int neg_count = 0, max_subarray = -1001 ;
         int tab[200001] = {0} ;
-        for (int i = 0 ; i<n ;i++)
+        for (int i = 0 ; i<n ;i++){
             cin>>tab[i] ;
-        int left = 0, right = 0 ;
-        int current_max = tab[0], max_subarray = tab[0] ;
-        while (right < n-1){
-            if (abs(tab[right + 1]%2) == abs(tab[right]%2)){
-                current_max = 0 ;
-                left = right + 1;
+            if (tab[i]<=0){
+                neg_count++ ;
             }
-            else{
-                while (left<=right && tab[left]<=0){
-                    current_max-=tab[left] ;
-                    left++ ;
+            max_subarray = max(tab[i], max_subarray) ;
+        }
+        if (neg_count == n){
+            cout<<max_subarray<<endl ;
+        }
+        else{
+            int left = 0, right = 0 ;
+            int current_max = tab[0] ;
+            max_subarray = tab[0] ;
+            while (right < n){
+                if (right +1 < n && abs(tab[right + 1]%2) == abs(tab[right]%2)){
+                    max_subarray = max(max_subarray, current_max) ;
+                    current_max = 0;
+                    left = right + 1 ; // left = right - 1 ;
                 }
+                else{
+                    while (left<=right && tab[left]<=0){
+                        current_max-=tab[left] ;
+                        left++ ;
+                    }
+                }
+                right++ ;
+                current_max+=tab[right] ;
+                max_subarray = max(max_subarray, current_max) ;
             }
-            right++ ;
-            current_max+=tab[right] ;
-            max_subarray = max(max_subarray, current_max) ;
+
+            for (int j = left ; j<right ; j++){
+                
+                current_max-=tab[j] ;
+                max_subarray = max(max_subarray, current_max) ;
+            }
+            cout<<max_subarray<<endl ;
         }
-        for (int j = left ; j<right ; j++){
-            
-            current_max-=tab[j] ;
-            max_subarray = max(max_subarray, current_max) ;
-        }
-        cout<<max_subarray<<endl ;
     }
 }
