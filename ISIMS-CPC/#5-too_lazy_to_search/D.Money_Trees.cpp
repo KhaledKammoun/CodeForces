cbf2786c1285256e50b1b5ef7fc17306b61b0592
KhaledKammoun <khaledsfax03@gmail.com>
2023-12-20 16:20:50 +0100

    C++

---
+++ ISIMS-CPC/#5-too_lazy_to_search/D.Money_Trees.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std ;
+int main(){
+    int t ;
+    t = 1 ;
+    cin>>t ;
+    while(t--){
+        int n, k ;
+        cin>>n>>k ;
+        int t_nbFruits[200001] = {0} ;
+        int t_length[200001] = {0} ;
+        for (int i = 0; i<n ;i++){
+            cin>>t_nbFruits[i] ;
+        }
+        for (int i = 0; i<n ;i++){
+            cin>>t_length[i] ;
+        }
+
+        int curr = t_nbFruits[0], max_length = 0, curr_lenght = 0;
+        if (curr <= k){
+            curr_lenght = 1 ;
+            max_length = 1 ;
+        }
+        for (int i = 0 ;i<n ;i++){
+            if (i<n-1 && t_length[i] % t_length[i+1] == 0 && curr + t_nbFruits[i] <= k){
+                curr_lenght++ ;
+                curr+=t_nbFruits[i] ;
+            }else{
+                max_length = max(max_length, curr_lenght) ;
+                curr = t_nbFruits[i] ;
+                if (curr <= k)
+                    curr_lenght = 1 ;
+            }
+            max_length = max(max_length, curr_lenght) ;
+        }
+        cout<<max_length<<endl ;
+    }
+}
