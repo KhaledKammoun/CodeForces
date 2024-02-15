34b554a957d9ade8ea06eaaab70f165ba2621157
KhaledKammoun <khaledsfax03@gmail.com>
2024-02-15 19:38:36 +0100

    C++

--- ISIMS-CPC/#8-L0ve_in_B1ts/D. Points and Powers of Two.cpp
+++ ISIMS-CPC/#8-L0ve_in_B1ts/D. Points and Powers of Two.cpp
@@ -2,36 +2,28 @@
 using namespace std ;
 vector<int> result {1};
 bool verif(vector<int>&v){
-    for (int i = 0; i < v.size(); i++){
-        for (int j = i + 1; j < v.size(); j++){
-            int var = abs(v[i] - v[j]) ;
-            int var_length = 0 ;
-            while (var){
-                var_length++ ;
-                var>>=2 ;
-            }
-            if (!(abs(v[i] - v[j]) & (2 << (var_length-1))))
-                return false ; 
+    for (int i = 0; i < v.size() - 1; i++){
+        int j = v.size() - 1 ;
+        int var = abs(v[i] - v[j]) ;
+        int var_length = 0 ;
+        while (var){
+            var_length++ ;
+            var>>=1 ;
         }
+        if (abs(v[i] - v[j]) != (1 << (var_length-1)))
+            return false ; 
     }
     return true ;
 }
 
-void dfs(int t,const vector<int>& v,vector<int> &subset, int start){
-    if (start>=t){
-        return ;
-    }
+void dfs(int t,vector<int>& v,vector<int> &subset, int start){
     for (int i = start; i < t; i++){
         subset.push_back(v[i]) ;
         if (!verif(subset)){
             subset.pop_back();
             continue;
         }
-        if (verif(subset) && result.size() < subset.size()){
-            for (int j = 0; j < subset.size(); j++){
-                cout<<subset[j]<<" " ;
-            }
-            cout<<endl ;
+        if (verif(subset) && result.size() <= subset.size()){
             result = subset ;
         }
         dfs(t, v, subset, i + 1) ;
