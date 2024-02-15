db8d3366ae4be5b6a28b530901881aac032bc0d7
KhaledKammoun <khaledsfax03@gmail.com>
2024-02-15 20:01:33 +0100

    C++

--- ISIMS-CPC/#8-L0ve_in_B1ts/D. Points and Powers of Two.cpp
+++ ISIMS-CPC/#8-L0ve_in_B1ts/D. Points and Powers of Two.cpp
@@ -1,48 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std ;
-vector<int> result {1};
-bool verif(vector<int>&v){
-    for (int i = 0; i < v.size() - 1; i++){
-        int j = v.size() - 1 ;
-        int var = abs(v[i] - v[j]) ;
-        int var_length = 0 ;
-        while (var){
-            var_length++ ;
-            var>>=1 ;
-        }
-        if (abs(v[i] - v[j]) != (1 << (var_length-1)))
-            return false ; 
-    }
-    return true ;
-}
-
-void dfs(int t,vector<int>& v,vector<int> &subset, int start){
-    for (int i = start; i < t; i++){
-        subset.push_back(v[i]) ;
-        if (!verif(subset)){
-            subset.pop_back();
-            continue;
-        }
-        if (verif(subset) && result.size() <= subset.size()){
-            result = subset ;
-        }
-        dfs(t, v, subset, i + 1) ;
-        subset.pop_back() ;
-    }
-    return ;
-}
 int main(){
 
     int t ;
     cin>> t ;
-    vector<int>v ;
+    int max_val = 0;
+    vector<int> result ;
+    unordered_map<int, bool> visited ;
+    vector<int> v ;
     for (int i = 0; i < t ;i++){
         int n ;
         cin>>n ;
+        visited[n] = true ;
+        max_val = max(max_val, n) ;
         v.push_back(n) ;
     }
-    vector<int> subset;
-    dfs(t, v, subset, 0) ;
+    
+    for (int i = 0; i < t; i++){
+        vector<int> subvector ;
+        int incremented_var = 0 ;
+        
+        while (abs(v[i] + ((incremented_var) ? (1<<incremented_var) : 0)) <= max_val){
+            auto it = (visited.find(abs(v[i] + ((incremented_var) ? (1<<incremented_var) : 0))));
+            if (it != visited.end()) {
+                subvector.push_back(abs(v[i] + ((incremented_var) ? (1<<incremented_var) : 0))) ;
+            }
+            incremented_var++ ;
+        }
+        if (result.size() <= subvector.size())
+            result = subvector ;
+    }
+
     cout<<result.size()<<endl ;
     for (int i = 0; i < result.size() ; i++)
         cout<<result[i]<<" " ;
