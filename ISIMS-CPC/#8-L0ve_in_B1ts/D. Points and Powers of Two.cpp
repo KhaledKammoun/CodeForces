3ea7d33f103d89db47b255d50f4999a1c6a947ae
KhaledKammoun <khaledsfax03@gmail.com>
2024-02-16 21:39:09 +0100

    C++

--- ISIMS-CPC/#8-L0ve_in_B1ts/D. Points and Powers of Two.cpp
+++ ISIMS-CPC/#8-L0ve_in_B1ts/D. Points and Powers of Two.cpp
@@ -1,37 +1,74 @@
 #include <bits/stdc++.h>
-using namespace std ;
-int main(){
+using namespace std;
 
-    int t ;
-    cin>> t ;
-    long long max_val = 0;
-    vector<long long> result ;
-    unordered_map<long long, bool> visited ;
-    vector<long long> v ;
-    for (int i = 0; i < t ;i++){
-        long long n ;
-        cin>>n ;
-        visited[n] = true ;
-        max_val = max(max_val, n) ;
-        v.push_back(n) ;
-    }
+unordered_map<long long, long long> lengths;
+
+bool verif(vector<long long> v, int val) {
     
-    for (int i = 0; i < t; i++){
-        vector<long long> subvector ;
-        long long incremented_var = 0 ;
-        
-        while (abs(v[i] + ((incremented_var < 63) ? (1LL << incremented_var) : 0)) <= max_val){
-            auto it = (visited.find(abs(v[i] + ((incremented_var < 63) ? (1LL << incremented_var) : 0))));
-            if (it != visited.end()) {
-                subvector.push_back(abs(v[i] + ((incremented_var < 63) ? (1LL << incremented_var) : 0))) ;
+    for (int i = 0; i < v.size(); i++) {
+        long long var_1 = abs(v[i] - val);
+        auto it = lengths.find(var_1);
+        if (it == lengths.end()){
+            int length = 0;
+            while (var_1) {
+                length++;
+                var_1 >>= 1;
             }
-            incremented_var++ ;
+            lengths[var_1] = length - 1;
         }
-        if (result.size() <= subvector.size())
-            result = subvector ;
+        if (abs(v[i] - val) != (1LL << (lengths[var_1]))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+int main() {
+    int t;
+    cin >> t;
+    long long max_val = 0;
+    vector<long long> result;
+    unordered_map<long long, bool> visited;
+    vector<long long> v;
+    
+    for (long long i = 0; i < t; i++) {
+        long long n;
+        cin >> n;
+        visited[n] = true;
+        max_val = max(max_val, n);
+        v.push_back(n);
+        // Precalculate lengths
+ 
     }
 
-    cout<<result.size()<<endl ;
-    for (int i = 0; i < result.size() ; i++)
-        cout<<result[i]<<" " ;
+    for (int i = 0; i < t; i++) {
+        vector<long long> subvector;
+        long long incremented_var = 0;
+        long long var_abs = v[i] + (1LL << incremented_var);
+        long long var_abs_1 = v[i] - (1LL << incremented_var);
+        subvector.push_back(v[i]);
+        
+        while (var_abs <= max_val && var_abs_1 <= max_val) {
+            auto it = visited.find(var_abs);
+            if (it != visited.end() && var_abs <= max_val && verif(subvector, var_abs)) {
+                subvector.push_back(var_abs);
+            }
+            auto it_1 = visited.find(var_abs_1);
+            if (it_1 != visited.end() && it == visited.end() && var_abs_1 <= max_val && verif(subvector, var_abs_1)) {
+                subvector.push_back(var_abs_1);
+            }
+            incremented_var++;
+            var_abs = v[i] + (1LL << incremented_var);
+            var_abs_1 = v[i] - (1LL << incremented_var);
+        }
+        
+        if (result.size() <= subvector.size())
+            result = subvector;
+    }
+
+    cout << result.size() << endl;
+    for (int i = 0; i < result.size(); i++)
+        cout << result[i] << " ";
 }
