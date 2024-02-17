ddff2b8b4e52eeb27914170b7112d130276de3dd
KhaledKammoun <khaledsfax03@gmail.com>
2024-02-17 15:05:04 +0100

    C++

---
+++ ISIMS-CPC/#8-L0ve_in_B1ts/A. Mocha and Math.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false);
+
+using namespace std ;
+
+
+
+int main(){
+    fast ;
+    int t ;
+    cin>>t ;
+    while (t--){
+        int n ;
+        cin>>n ;
+        int result ;
+        for (int i = 0; i < n ; i++){
+            int var ;
+            cin>>var ;
+            if (i == 0)
+                result = var ;
+            else {
+                result^=var ;
+            }
+        }
+        cout<<result<<endl ;
+    }
+}
