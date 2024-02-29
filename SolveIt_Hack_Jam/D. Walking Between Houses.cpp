5c3d8733b9364664340eddb55a8e70d4ae649d2a
KhaledKammoun <khaledsfax03@gmail.com>
2024-02-29 17:52:25 +0100

    C++

--- SolveIt_Hack_Jam/D. Walking Between Houses.cpp
+++ SolveIt_Hack_Jam/D. Walking Between Houses.cpp
@@ -12,20 +12,35 @@ int main(){
     cin>>n>>k>>s ;
     int new_s = 0 ;
     n-- ;
+    s-- ;
     vector<int> result ;
     for (int i = 0; i < k && s > 0; i++){
-        new_s = abs(new_s - ((s<n) ? s : n)) ;
-        s-=((s<n) ? s : n) ;
+        if ((s > n) || (s == n && i == k)){
+            new_s = abs(n - new_s) ;
+            s-=n ;
+        }
+        else if (s < n) {
+            if (i == k-1){
+                new_s = abs(n - s) - 1;
+                s = 0;
+            }
+            else {
+                new_s = abs(new_s - 1) ;
+                s-- ;
+            }
+        }
         result.push_back(new_s+1) ;
     }
-    if (result.size() == k && s == 0){
+    if (s == 0){
         cout<<"YES"<<endl ;
-        for (int i = 0; i < k; i++)
-            cout<<result[i]<<" " ;
+
     }
     else {
         cout<<"NO" ;
     }
+    for (int i = 0; i < k; i++)
+        cout<<result[i]<<" " ;
+    cout<<endl<<s ;
 
 }
 
